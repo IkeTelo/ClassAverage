43289cb5145299f19c64682862baf420748d8699
Isaac Telloian <iketelloian@fresnostate.edu>
2022-03-27 13:09:51 -0700

    first commit

---
+++ class_grade.py
@@ -0,0 +1,10 @@
+""" PLAIN ENGLISH
+start
+create a list to store 5 number (float)
+capture user's input 5 times for their grades
+each time we capture the user's input, we append the number to the list
+sort the list ascending, then splice the items starting at index 2
+once we have three highst number in the list, we sum them up and divide by 3
+output a message to the user
+end
+"""
