dd0498f658c1bd1ac96a9261d9ad6285ae51b9f2
Isaac Telloian <iketelloian@fresnostate.edu>
2022-03-27 13:44:32 -0700

    finish master

--- class_grade.py
+++ class_grade.py
@@ -8,3 +8,48 @@ once we have three highst number in the list, we sum them up and divide by 3
 output a message to the user
 end
 """
+"""PSEUDOCODE
+create list
+
+capture input
+append number to list
+
+capture input
+append to list
+
+capture input
+append number to list
+
+capture input
+append to list
+
+capture input
+append to list
+
+sort the list, then splice out the two lowest number
+print message to user
+"""
+
+grades = []
+
+grade = input("Enter the 1st grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 2nd grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 3rd grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 4th grade: ")
+grades.append(float(grade))
+
+grade = input("Enter the 5th grade: ")
+grades.append(float(grade))
+
+grades.sort()
+grades = grades[2 :]
+grades = sum(grades)
+result = grades / 3
+
+print("Average Grade {0:.2f}%".format(result))
